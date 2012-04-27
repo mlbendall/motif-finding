a444d2bf27389d76752cb939a8b9735d66654927
Patrick Marks <pmarks@pacificbiosciences.com>
2012-04-27 12:03:08 -0700

    genome annotation bug fixes, checking in pdf

--- scripts.R
+++ scripts.R
@@ -1,3 +1,5 @@
+library(Biostrings)
+
 # Parse an attirubte field from the GFF attributes field by name
 getAttributeField <- function (x, field, attrsep = ";") {
   s = strsplit(x, split = attrsep, fixed = TRUE)
@@ -16,7 +18,7 @@ getAttributeField <- function (x, field, attrsep = ";") {
 
 # Load a GFF file into a data.frame.  The attributes column is left alone
 gffRead <- function(gffFile, nrows = -1) {
-  cat("Reading ", gffFile, ": ", sep="")
+  cat("Reading ", gffFile, sep="")
   gff = read.table(gffFile, sep="\t", as.is=TRUE, quote="",
                    header=FALSE, comment.char="#", nrows = nrows,
                    colClasses=c("character", "character", "character", "integer",  
@@ -25,16 +27,13 @@ gffRead <- function(gffFile, nrows = -1) {
   
   colnames(gff) = c("seqname", "source", "feature", "start", "end",
                     "score", "strand", "frame", "attributes")
-  
-  cat("found", nrow(gff), "rows with classes:", paste(sapply(gff, class), collapse=", "), "\n")
-  stopifnot(!any(is.na(gff$start)), !any(is.na(gff$end)))
   return(gff)
 }
 
 
 # Helper to load a PacBio modifications.gff file.  Parse some custom attribute fields relevant
 # for modification detection.
-readModsGff <- function(gffFile, nrows=-1)
+readModificationsGff <- function(gffFile, nrows=-1)
 {
   gffData <- gffRead(gffFile, nrows)
   gffData$coverage <- as.numeric(getAttributeField(gffData$attributes, 'coverage'))
@@ -43,18 +42,13 @@ readModsGff <- function(gffFile, nrows=-1)
   gffData
 }
 
-
-
-
-
 # example set of motifs to label
 
 # Sequence of motifs
-motifs <- c('CGACCAG', 'GANTC', 'CGACNNNNNNNTRGG', 'CCYANNNNNNNGTCG')
+# motifs <- c('CGACCAG', 'gantc', 'CGACNNNNNNNTRGG', 'CCYANNNNNNNGTCG')
 
 # Modified position in motif
-motifSites <- c(6,2,3,4)
-
+# motifSites <- c(6,2,3,4)
 
 # Label each element of contextString as containing a motifs, or 'None'
 # motifs is a list of motif strings, possibly including ambiguous bases
@@ -78,4 +72,74 @@ labelContexts <- function(contextStrings, motifs, motifSites, labels=motifs)
   factor(labelVect)
 }
 
-labels <- labelContexts(hits$context, motifs, motifSites)
+
+
+# Generate a data.frame with one row for each example of motif in the reference
+# meth pos is the position of the m6A in the motif, offset=0 means to expect a 
+# peak at the metylated position, offset=-5 is the normal position of the 
+# secondary peak for m6A.  If there are Ns in the motif tb.end must be set 
+# to the end of the first block of non-Ns
+annotationDf <- function(dna_seq, motifs, positions, offsets=c(0))
+{
+  df <- data.frame()
+  dna_seq_rc <- reverseComplement(dna_seq)
+  
+  for (ref in 1:length(dna_seq))
+  {
+    for(m in 1:length(motifs))
+    {  
+      motif <- motifs[m]
+      methPos <- positions[m]
+      
+      maskFwd <- mask(dna_seq[[ref]], 'N')      
+      maskRev <- mask(dna_seq_rc[[ref]], 'N')
+      
+      match_fwd <- matchPattern(motif, maskFwd, fixed=F)
+      match_rev <- matchPattern(motif, maskRev, fixed=F)  
+      
+      for (o in offsets)
+      {
+        modName <- motif
+        if(o > 0)
+          modName = paste(motif, "p", o, sep='')
+        if(o < 0)
+          modName = paste(motif, "n", o, sep='')
+        
+        onTarget <- 'On'
+        if(abs(o) > 0)
+          onTarget <- 'Off'
+        
+        
+        fwdDf <- as.data.frame(match_fwd)
+        if(nrow(fwdDf) > 0)
+        {
+          fwdDf$contigId <- ref
+          #fwdDf$contig <- names(dna_seq)[[ref]]
+          
+          fwdDf$strand <- 0
+          fwdDf$tpl <- fwdDf$start + methPos - 1 + o
+          fwdDf$mod <- modName
+          fwdDf$onTarget <- onTarget  
+          df <- rbind(df, fwdDf)
+        }
+        
+        revDf <- as.data.frame(match_rev)
+        if(nrow(revDf) > 0)
+        {
+          revDf$contigId <- ref
+          #revDf$contig <- names(dna_seq)[[ref]]
+          
+          revDf$strand <- 1
+          revDf$tpl <- length(dna_seq[[1]]) + 1 - (revDf$start + methPos - 1 + o)
+          revDf$mod <- modName
+          revDf$onTarget <- onTarget
+          df <- rbind(df, revDf)
+        }
+      }
+    }
+  }
+  
+  df <- df[,c('strand', 'tpl', 'mod',  'onTarget', 'contigId')]
+  return(df)
+}
+
