6e5e38d1b1054285663cb475457b6ae2a2a145b4
Khai Luong <kluong@pacificbiosciences.com>
2012-05-02 18:02:34 -0700

    2 changes.
    1.	Fasta write of remains has ids as refpos+strand instead of sequential 1 to 1000
    
    2.	Added plotting functionality so one can choose to plot ipdRatio, score, or coverage by range of positions. And dumps into a pdf file.

--- scripts.R
+++ scripts.R
@@ -1,4 +1,5 @@
 library(Biostrings)
+library(grid)
 
 # Parse an attirubte field from the GFF attributes field by name
 getAttributeField <- function (x, field, attrsep = ";") {
@@ -42,6 +43,14 @@ readModificationsGff <- function(gffFile, nrows=-1)
   gffData$attributes <- NULL
   
   gffData
+}
+
+filterGff <- function(gff,idx,labels,fOut)
+{
+  gffDat <- gffRead(gff)
+  gffDat <- gffDat[idx,]
+  gffDat$motif <- labels
+  write.table(file=fOut,gffDat,row.names=F,col.names=T,sep=' ')
 }
 
 # example set of motifs to label
@@ -145,10 +154,87 @@ genomeAnnotation <- function(dna_seq, motifs, positions, offsets=c(0))
 }
 
 # Write the contextStrings to a FASTA file
-writeContextFasta <- function(contextStrings, filename)
+writeContextFasta <- function(dat, filename)
 {
-  stringSet <- DNAStringSet(contextStrings)
-  names(stringSet) <- 1:length(contextStrings)
+  stringSet <- DNAStringSet(dat$context)
+  names(stringSet) <- paste(as.character(dat$start),dat$strand,sep='')
   write.XStringSet(stringSet, filename)
 }
 
+# Plot kinetics = c('ipdRatio','score','coverage')
+plotKinetics <- function(dat,ref,statMode,plotRange,rangePerPlot)
+{
+  dat <- subset(dat,seqid=ref)
+  tableNames <- names(dat)
+  idx <- match(statMode,tableNames)
+  posIdx <- match('tpl',tableNames)
+  baseIdx <- match('base',tableNames)
+  plotTitle <- statMode
+  fontSize <- floor(800/rangePerPlot)
+  baseline <- median(dat[,idx],na.rm=TRUE)
+  if (statMode =='coverage') baseline <- 0
+  yFwdStr <- 'Forward Strand'
+  yRevStr <- 'Reverse Strand'
+ 
+  posRange <- plotRange
+  nPlots <- ceiling((posRange[2]-posRange[1])/rangePerPlot)
+  
+  for (n in 1:nPlots)
+  {
+    sWin <- (n-1)*rangePerPlot+posRange[1]
+    eWin <- sWin+rangePerPlot
+    xbin <- 10
+    subDat <- subset(dat,tpl >= sWin & tpl <= eWin)
+    fwdDat <- subDat[subDat$strand=='+',c(posIdx,idx,baseIdx)]
+    revDat <- subDat[subDat$strand=='-',c(posIdx,idx,baseIdx)]
+    yMin <- -0.5
+    yMax <- max(subDat[,idx],na.rm=TRUE)*1.05
+    if (yMax < 5) yMax = 5
+    plotDat <- data.frame(tpl = sWin:eWin, statFwd = baseline, statRev = baseline)
+    plotDat$baseRev = "x"
+    plotDat$baseFwd = "x"
+    
+    posMatch <- match(plotDat$tpl,fwdDat$tpl)
+    fwdIdx <- posMatch[which(is.na(posMatch)==FALSE)]
+    plotDat$statFwd[!is.na(posMatch)] = fwdDat[fwdIdx,2]
+    plotDat$baseFwd[!is.na(posMatch)] = as.character(fwdDat$base[fwdIdx])
+    
+    posMatch <- match(plotDat$tpl,revDat$tpl)
+    revIdx <- posMatch[which(is.na(posMatch)==FALSE)]
+    plotDat$statRev[!is.na(posMatch)] = revDat[revIdx,2]
+    plotDat$baseRev[!is.na(posMatch)] = as.character(revDat$base[revIdx])
+    plotDat$baseline <- baseline
+    
+    p1 <- ggplot(data=plotDat, aes(xmin=tpl-0.4,xmax=tpl+0.4,ymin=baseline,ymax=statFwd))+geom_rect(fill='salmon',label='')+ylab(yFwdStr)+ylim(c(yMin,yMax))+ scale_x_continuous(breaks=seq(sWin,eWin,xbin))+opts(axis.text.x=theme_blank(),legend.position = "none",title=plotTitle,plot.margin= unit(c(0, 2, -.5, 0), "lines"))+coord_cartesian(xlim = c((sWin-1),(eWin+1)))
+    pp1 <- p1 + geom_text(aes(x=tpl, y=0, vjust=1, label=toupper(baseFwd)), size=fontSize)+opts(axis.text.x = theme_blank(),axis.label.x = theme_blank())
+    
+    p2 <- ggplot(data=plotDat, aes(xmin=tpl-0.4,xmax=tpl+0.4,ymin=baseline,ymax=statRev))+geom_rect(fill='salmon',label='')+xlab('')+ylab(yRevStr)+ylim(c(yMax,yMin))+ scale_x_continuous(breaks=seq(sWin,eWin,xbin))+opts(legend.position = "none",plot.margin= unit(c(-0.5, 2, 0, 0), "lines"))+xlab('Ref Position')+coord_cartesian(xlim = c((sWin-1),(eWin+1)))
+    pp2 <- p2 + geom_text(aes(x=tpl, y=0, vjust=0, label=toupper(baseRev)), size=fontSize)
+    
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow = 2, ncol = 1)))
+    print(pp1, vp = viewport(layout.pos.row = 1, layout.pos.col = 1))
+    print(pp2, vp = viewport(layout.pos.row = 2, layout.pos.col = 1))
+  }
+}
+
+readModCsv <- function(csvFile,genomeSize,chunkSize)
+{
+  fileSize <- genomeSize*2
+  nchunks <- ceiling(fileSize/chunkSize)
+  tmp <- read.table(file=csvFile,header=TRUE,sep=',',skip=0,nrow=5)
+  colClasses <- sapply(tmp, class)
+  csvHeader <- names(tmp)
+  keepCols <- c('refId','tpl','strand','ipdRatio','pvalue','coverage')
+  theTable <- NULL
+  for (n in 1:nchunks)
+  {   
+    sk = (n-1)*chunkSize+1
+    last = sk+chunkSize
+    if (last > fileSize) chunkSize <- fileSize-sk
+    tmp <- read.table(file=csvFile,header=FALSE,sep=',',skip=sk,nrow=chunkSize,colClasses=colClasses)
+    names(tmp) <- csvHeader
+    theTable <- rbind(theTable,tmp[,keepCols])
+  }
+  return(theTable)  
+}
